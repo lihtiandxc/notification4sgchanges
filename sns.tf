ab2f019e116977cae5868e7b80b2cc9f600a8f07
LT <llt@hpe.com>
2018-03-06 16:32:56 +0800

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -7,7 +7,7 @@ variable "sns_disp" {
 
 locals {
   local-exec01  = "aws sns subscribe --topic-arn ${aws_sns_topic.management.arn} --protocol email --notification-endpoint ${var.name["fr-aism-mailing"]}"
-  local-exec02  = "aws sns subscribe --topic-arn ${aws_sns_topic.management.arn} --protocol email --notification-endpoint ${var.name["dxcr-aism-mailing"]}"
+  local-exec02  = "aws sns subscribe --topic-arn ${aws_sns_topic.management.arn} --protocol email --notification-endpoint ${var.name["dxc-aism-mailing"]}"
 }
 #variable "commands" {
 #  default {
