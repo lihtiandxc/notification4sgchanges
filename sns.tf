9e0858df6d0c765d005b482eddeebedbd53d0684
LT <llt@hpe.com>
2018-03-06 17:56:15 +0800

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -5,34 +5,37 @@ variable "sns_disp" {
   }
 }
 
+resource "aws_sns_topic" "management" {
+  name         = "${var.sns_disp["sns_mng"]}"
+  display_name = "${var.sns_disp["sns_mng"]}"
+  provider     = "aws.virginia"
+}
+
+module "sns_topic_arn" {
+  sns_topic_arn = ${aws_sns_topic.management.id}
+}
+
 locals {
-  local-exec01  = "aws sns subscribe --topic-arn ${output.sns_topic_arn} --protocol email --notification-endpoint ${var.name["fr-aism-mailing"]}"
+  local-exec01  = "aws sns subscribe --topic-arn ${module.sns_topic_arn.sns_topic_arn} --protocol email --notification-endpoint ${var.name["fr-aism-mailing"]}"
   #local-exec02  = "aws sns subscribe --topic-arn ${aws_sns_topic.management.arn} --protocol email --notification-endpoint ${var.name["dxc-aism-mailing"]}"
 }
-#variable "commands" {
-#  default {
-#    local-exec01  = "aws sns subscribe --topic-arn ${aws_sns_topic.managemetn.arn} --protocol email --notification-endpoint ${var.name["fr-aism-mailing"]}"
-#    local-exec02  = "aws sns subscribe --topic-arn ${aws_sns_topic.managemetn.arn} --protocol email --notification-endpoint ${var.name["dxc-aism-mailing"]}"
-#  }
-#}
 
+resource "null_resource" "subscription" {
+  provisioner "local-exec" {
+    command = "${local.local-exec01}"
+  }
+
+}
+
+#workable
+/*
 resource "aws_sns_topic" "management" {
   name         = "${var.sns_disp["sns_mng"]}"
   display_name = "${var.sns_disp["sns_mng"]}"
   provider     = "aws.virginia"
 
   provisioner "local-exec" {
-    #command = "${var.command["local-exec01"]}"
-    #command = "${var.command["local-exec02"]}"
-    command = "${local.local-exec01}"
+    command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint ${var.name["dxc-aism-mailing"]};aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint ${var.name["fr-aism-mailing"]}"
   }
 }
-
-output "sns_topic_arn" {
-  value = "${aws_sns_topic.management.arn}"
-}
-
-
-#aws cli create new subscription
-
-#aws sns subscribe --topic-arn arn:aws:sns:us-west-2:0123456789012:my-topic --protocol email --notification-endpoint my-email@example.com
+*/
