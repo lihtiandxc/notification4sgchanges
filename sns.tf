9e9fce4f30e8bc61aa858cdd33399c40b6c22946
LT <llt@hpe.com>
2018-03-06 15:02:20 +0800

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -19,7 +19,8 @@ resource "aws_sns_topic" "management" {
   provider     = "aws.virginia"
 
   provisioner "local-exec" {
-    command = "${var.command["local-exec01"]}"
+    command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint ${var.name["fr-aism-mailing"]}"
+    #command = "${var.command["local-exec01"]}"
     #command = "${var.command["local-exec02"]}"
   }
 }
